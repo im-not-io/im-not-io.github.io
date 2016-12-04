15945a229dedb16b3aefa3759ac9b1d42cb3a246
Nick DiGeronimo <Nick DiGeronimo>
2016-12-04 07:26:14 -0800

    version 2.0 site progress

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,19 +1,25 @@
+var currentIndex = 0;
+
 function toggleAboutMeDescription() {
             if (document.getElementById("mainContentBox").className === "mainContentBox visible") {
                 document.getElementById("mainContentBox").className = "mainContentBox hidden";
             } else {
                 document.getElementById("mainContentBox").className = "mainContentBox visible";
             }
-                        console.log(document.getElementById("mainContentBox").className)
-
         }
-  
-        document.addEventListener("DOMContentLoaded", function(event) {
+            window.addEventListener("load", function(anotherEvent) {   
+            document.body.className = "loadBackgroundImages";
+            var imagesProperties = window.getComputedStyle(document.body).backgroundImage.split(", ");
+            console.log(imagesProperties);
+            window.addEventListener("keydown", function(event) {
+                if (event.keyCode == 32) {
+                    currentIndex = ++currentIndex % imagesProperties.length
+                    document.body.style.backgroundImage = imagesProperties[currentIndex];
+                    console.log(imagesProperties[currentIndex]);
+                }
+            });
 
             document.getElementById("learnMoreAboutMeLink").addEventListener("click", function() {
                 toggleAboutMeDescription();
             });
         });
-
-
-
