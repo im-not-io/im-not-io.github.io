5c3b68fd790df14749ae30f3bd48646336176b13
Nick DiGeronimo <Nick DiGeronimo>
2016-12-07 14:27:36 -0800

    a

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -18,8 +18,8 @@ var currentIndex = 0;
                 bg.style.backgroundImage = imageProperties[Math.floor(Math.random() * imageProperties.length)];
             //bg.style.height = window.innerHeight + "px";
             //bg.style.width = window.innerWidth + "px";
-            bg.style.maxHeight = window.innerHeight + "px";
-            bg.style.maxWidth = window.innerWidth + "px";
+            //bg.style.maxHeight = window.innerHeight + "px";
+            //bg.style.maxWidth = window.innerWidth + "px";
             //bg.style.backgroundSize = "cover";
             });
             window.addEventListener("load", function(anotherEvent) {
