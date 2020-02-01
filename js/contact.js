bc93d1243e35fe0a3d46d8259a2d69c481f87b7c
im-not-io <perfectalgorithm@gmail.com>
2020-01-31 18:05:51 -0800

    changing forwarding email, correcting type

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-        document.getElementById("email").href += "mailto:forwarded.to.nick.digeronimo@gmail.com" + "@gm" + "ail." + "com";
-        document.getElementById("email").textContent += "forwarded.to.nick.digeronimo@gmail.com" + "@gm" + "ail." + "com";
+        document.getElementById("email").href += "mailto:forwarded.to.nick.digeronimo" + "@gm" + "ail." + "com";
+        document.getElementById("email").textContent += "forwarded.to.nick.digeronimo" + "@gm" + "ail." + "com";
 });
